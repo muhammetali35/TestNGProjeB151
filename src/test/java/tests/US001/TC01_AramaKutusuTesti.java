4b81041db5c371d12b7ccbf936c5cc7ffea548e8
muhammetali35 <129202058+muhammetali35@users.noreply.github.com>
2023-07-24 22:00:10 +0300

    US001_V1

---
+++ src/test/java/tests/US001/TC01_AramaKutusuTesti.java
@@ -0,0 +1,19 @@
+package tests.US001;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.Page;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class TC01_AramaKutusuTesti {
+    @Test
+    public void test01() {
+        Driver.getDriver().get(ConfigReader.getProperty("googleUrl"));
+        ReusableMethods.bekle(2);
+        Page page = new Page();
+        page.aramaKutusu.sendKeys("iphone", Keys.ENTER);
+
+    }
+}
---
+++ src/test/java/utilities/DataProviderUtil.java
@@ -0,0 +1,10 @@
+package utilities;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviderUtil {
+    @DataProvider
+    public static Object[][] sehirVerileri() {
+        return new Object[][]{{"Ankara","IcAnadolu","06"},{"Izmir","Ege","35"},{"Diyarbakir","Doğu","21"}};
+    }
+}
